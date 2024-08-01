bc3a923d52e4f4983289c46f2b6af26f50bbfb03
Aryak Gautam <123766175+aryakgautam@users.noreply.github.com>
2024-08-01 19:24:41 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")
+
+console.log(3+3) 
+console.log("Hitesh")
+
+let name = "aryak"
+let age = 20 
+let isLoggedIn = false 
+
+// number =>2 to power 53
+// bigint
+// string =""
+// boolean => true/false
+// null => standalone value
+// symbol => to find uniqueness 
+
+console.log(typeof null);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const account_Id = 144553;
+let account_email = "aryakgautam53@gmail.com";
+var account_password = "12345";
+var account_city = "delhi";
+
+// Update account details
+account_email = "aryak@gmail.com";
+account_password = "221103";
+account_city = "lucknow";
+
+
+console.log(account_email);
+console.table([account_Id,account_city,account_email,account_password])
